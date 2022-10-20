c3c5d1a8fbd99aa0981a855b8ff2ca7f4f29e4c0
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-10-20 14:07:09 +0200

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using webapp_travel_agency.Models;
+
+namespace webapp_travel_agency.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<SmartBox>? smartBoxes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=db_travel_agency;User=sa;Password=Scheggia12$;");
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
+
+
+
+
+
+
+
---
+++ Models/SmartBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+//https://picsum.photos/200/200
+namespace webapp_travel_agency.Models
+{
+    public class SmartBox
+    {
+       
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(75, ErrorMessage = "Il titolo non può essere oltre i 75 caratteri")]       
+        public string? Title { get; set; }
+
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [StringLength(800, ErrorMessage = "La Dscrizione non può essere oltre i 800 caratteri")]
+        [Column(TypeName = "text")]
+        public string? Description { get; set; }
+
+
+        [Required(ErrorMessage = "Il campo è obbligatorio")]
+        [Url]
+        public string? Image { get; set; }
+        
+
+
+        [Required(ErrorMessage = "Il Capo obbligatorio")]
+        [Range(1, 30, ErrorMessage = "La Durata durta deve essere compresa tra 1 a 30 notti")]
+        public int? DurataInNotti { get; set; }
+
+
+        [Required(ErrorMessage = "Il prezzo e' obbligatorio")]
+        [Range(1, 500, ErrorMessage = "Il prezzo della box deve essere incluso tra 1 e 500 euro")]
+        public decimal Price { get; set; }
+
+        public SmartBox()
+        {
+        }
+    }
+}
+
